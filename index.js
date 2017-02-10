9cd885fddfd59cb3483aff5a33e67641fe85d857
Sigurthor Bjorgvinsson <sigurthor12@ru.is>
2017-02-09 22:59:58 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var args = require("./lib/argParser")();
+var values = require("./lib/getValues")(args);
+
+require("./lib/execute.js")(values, function(results){
+	require("./lib/htmlGenerator.js")(results);
+});
+
+
+
