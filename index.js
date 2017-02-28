f70077968fd82190b24d2d04be9ca27085203cc4
Sigurthor Bjorgvinsson <sigurthor12@ru.is>
2017-02-27 20:54:16 -0500

    Changed htmlGenerator to return a object instead of function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var args = require("./lib/argParser")();
 var values = require("./lib/getValues")(args);
 
 require("./lib/execute.js")(values, function(results){
-	require("./lib/htmlGenerator.js")(results);
+	require("./lib/htmlGenerator.js").saveResultsSummary(results);
 });
 
 
